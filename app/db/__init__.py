c52184157165916461e3478e3c734fc6f46f9d88
Yoav Isaacs <yoav.isaacs@gmail.com>
2024-12-21 23:00:30 +0200

    [database] added functions to add to db

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -2,30 +2,30 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+def get_db():
+    load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_PORT = os.getenv("DB_PORT")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_PORT = os.getenv("DB_PORT")
 
-try:
-    connection = psycopg2.connect(
-        host=DB_HOST,
-        database=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        port=DB_PORT
-    )
+    try:
+        connection = psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            port=DB_PORT
+        )
 
-    cursor = connection.cursor()
+        yield connection
 
 
-except Exception as e:
-    print(f"Failed to connect\n{e}")
-finally:
-    if cursor:
-        cursor.close()
-    if connection:
-        connection.close()
+    except Exception as e:
+        print(f"Failed to connect\n{e}")
+    finally:
+        if connection:
+            connection.close()
+
